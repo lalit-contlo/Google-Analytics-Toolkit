eab62421689b1edc4feff2d8722f4d5e1ff1a9ee
Lalit Kumar Jena <lalit@contlo.com>
2023-07-21 15:11:43 +0530

    First commit

---
+++ google-analytics_toolkit.py
@@ -0,0 +1,15 @@
+from abc import ABC
+import os
+from superagi.tools.base_tool import BaseToolkit, BaseTool
+from typing import List
+from userReport import reportTool
+
+class AnalyticsToolkit(BaseToolkit, ABC):
+    name: str = "Google Analytics Toolkit"
+    description: str = "Google Analytics Toolkit returns google analytics reports requested by the user"
+
+    def get_tools(self) -> List[BaseTool]:
+        return [reportTool()]
+
+    def get_env_keys(self) -> List[str]:
+        return ["GOOGLE_APPLICATION_CREDENTIALS"]
